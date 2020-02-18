a8843644e9d507d1f5f920923f2c6538815df065
Chris Blyth <blythmeister@gmail.com>
2020-02-18 09:46:03 +0000

    When running in debugger, always force a enter to close debugger

--- src/PaketChain/Program.cs
+++ src/PaketChain/Program.cs
@@ -1,9 +1,28 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace PaketChain
 {
     internal class Program
     {
-        public static int Main(string[] args) => CommandLineApplication.Execute<RunnerArgs>(args);
+        public static async Task<int> Main(string[] args)
+        {
+            try
+            {
+                return await CommandLineApplication.ExecuteAsync<RunnerArgs>(args);
+            }
+            finally
+            {
+                if (Debugger.IsAttached)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("-----------------------------------------------------");
+                    Console.WriteLine("Press Enter To Close Debugger");
+                    Console.ReadLine();
+                }
+            }
+        }
     }
 }
